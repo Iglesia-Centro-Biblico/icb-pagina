8ef902bf937de5f7d54ada68d6685e1ae25b9ee1
Jorge Saucedo <jsaucedo@192.168.1.17>
2023-08-13 06:47:34 -0300

    fixed footer

--- src/components/footer/Footer.vue
+++ src/components/footer/Footer.vue
@@ -1,8 +1,17 @@
 <template>
     <footer id="footer" class="icb-footer">
         <div class="icb-footer__logos">
-            <img class="icb-footer__logos-icb" src="/logo-completo-blanco.svg" height="50" alt="Logo Footer">
+            <img class="icb-footer__logos-icb" src="/logo-completo-blanco.svg" height="80" alt="Logo Footer">
             <div class="icb-footer__logos-social-media">
+                <a href="https://www.facebook.com/IglesiaUCBPdu/" target="_blank">
+                    <img src="@/assets/icons/facebook.svg" width="50" height="50" alt="facebook" />
+                </a>
+                <a href="https://www.instagram.com/iglesiau.c.bpdu" target="_blank">
+                    <img src="@/assets/icons/instagram.svg" width="50" height="50" alt="instagram" />
+                </a>
+                <a href="https://www.youtube.com/@iglesiacentrosbiblicospdu5791" target="_blank">
+                    <img src="@/assets/icons/youtube.svg" width="50" height="50" alt="youtube" />
+                </a>
             </div>
         </div>
         <p class="icb-footer__copyright">© 2023 Iglesia Centro Bíblico Paysandú</p>
@@ -12,30 +21,55 @@
 </script>
 <style scoped lang="scss">
 .icb-footer {
+    position: relative;
     width: 100%;
     height: 200px;
     background-color: $secondary;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    align-items: center;
     &__logos {
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-        margin-top: 16px;
-        &__social-media {
+        height: 100%;
+        padding-bottom: 16px;
+        &-social-media {
             display: flex;
             justify-content: center;
             align-items: center;
+            gap: 8px;
+            margin-top: 12px;
         }
     }
     &__copyright {
+        position: absolute;
+        bottom: 8px;
+        width: 100%;
         font-size: 12px;
         color: #CCCCCC;
         justify-self: flex-end;
-        margin-bottom: 18px;
+        text-align: center;
+    }
+    @include q-medium {
+        height: 450px;
+        bottom: 16px;
+        &__logos {
+            flex-direction: row;
+            justify-content: space-around;
+            align-self: center;
+            &-icb {
+                height: 150px;
+            }
+            &-social-media {
+                gap: 16px;
+                a img {
+                    height: 130px;
+                    width: 130px;
+                }
+            }
+        }
+        &__copyright {
+            font-size: 24px;
+        }
     }
 }
 </style>
